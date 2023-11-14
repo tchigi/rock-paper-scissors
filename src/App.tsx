e63908c485e2f8864a71d1f15521b10c1add1acd
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2023-11-14 18:50:54 +0300

    Начало проекта

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {ChakraProvider} from "@chakra-ui/react";
+
+function App() {
+
+    return (
+        <ChakraProvider>
+            <div></div>
+        </ChakraProvider>
+    )
+}
+
+export default App
